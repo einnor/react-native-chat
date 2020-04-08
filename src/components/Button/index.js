67aa536c9bac26aacbd620bb14ff6e6a258d2bf7
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-04-08 19:19:07 +0300

    Updated button to show loading indicator

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,15 +1,27 @@
 import React from 'react';
-import { TouchableOpacity, Text } from 'react-native';
+import { TouchableOpacity, Text, ActivityIndicator, View } from 'react-native';
 
 import styles from './styles';
 
 const Button = (props) => {
-  const { title = 'Enter', style = {}, textStyle = {}, onPress } = props;
+  const { title = 'Enter', style = {}, textStyle = {}, onPress, isLoading } = props;
 
-  return (
+  const loader = () => {
+    return (
+      <ActivityIndicator animating={isLoading} />
+    );
+  };
+
+  const button = () => (
     <TouchableOpacity onPress={onPress} style={[styles.button, style]}>
       <Text style={[styles.text, textStyle]}> {title}</Text>
     </TouchableOpacity>
+  );
+
+  return (
+    <View style={[styles.button, style]}>
+      {isLoading ? loader() : button()}
+    </View>
   );
 };
 
