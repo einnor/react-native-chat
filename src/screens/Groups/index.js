14a9cf0c06a377c5c22480e6ab3caaae80820c20
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-04-08 16:23:52 +0300

    Groups screen

---
+++ src/screens/Groups/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+import styles from './styles';
+
+const Groups = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text} > Groups Screen</Text>
+    </View>
+  );
+};
+
+export default Groups;
