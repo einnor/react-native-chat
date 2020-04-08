a4d58fc4edde786f80e67dc7d5b43a1e962d344b
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-04-08 16:26:05 +0300

    Added styles for Chat screen

---
+++ src/screens/Chat/styles.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#ebebeb'
+  },
+  text: {
+    color: '#101010',
+    fontSize: 24,
+    fontWeight: 'bold'
+  }
+});
+
+export default styles;
