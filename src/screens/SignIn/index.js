cd55926ceb1386f1d12db01926a315da3c5d5336
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-04-08 17:11:17 +0300

    Testing

--- src/screens/SignIn/index.js
+++ src/screens/SignIn/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 
+import String from '../../consts/String';
+import Button from '../../components/Button';
 import styles from './styles';
 
 const SignIn = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.text} > SignIn Screen</Text>
+      <Button title={String.Join}></Button>
     </View>
   )
 };
