4dc0b304b686a59bcca248f3c314e414bde1b66e
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-04-08 17:08:01 +0300

    Added TextField component

---
+++ src/components/TextField/index.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { TextInput, Text, View } from 'react-native'
+
+
+const TextField = ({ term, placeHolder, OnTermChange, onValidateEmailAddress, error }) => {
+
+  return (
+    <View>
+      <Text style={styles.error}> {error}</Text>
+      <View style={styles.TextFieldView}>
+        <TextInput
+          autoCorrect={false}
+          style={styles.TextField}
+          placeholder={placeHolder}
+          value={term}
+          onChangeText={OnTermChange}
+          onEndEditing={onValidateEmailAddress}
+        />
+      </View>
+    </View>
+  );
+};
+
+export default TextField;
