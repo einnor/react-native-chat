26c67d197280d957b5f674e3c00ec74528f19a89
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-04-08 16:24:51 +0300

    Add Group screen

---
+++ src/screens/AddGroup/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+import styles from './styles';
+
+const AddGroup = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text} > Add Group</Text>
+    </View>
+  );
+};
+
+export default AddGroup;
