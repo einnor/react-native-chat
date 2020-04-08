02b92ebc260da190034852695773681aab0ccb76
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-04-08 16:20:08 +0300

    SignIn screen

---
+++ src/screens/SignIn/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+import styles from './styles';
+
+const SignIn = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text} > SignIn Screen</Text>
+    </View>
+  )
+};
+
+export default SignInScreen;
