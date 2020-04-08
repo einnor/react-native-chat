faf6f8f3e5fd1883e231a6518dd90a10a2d32a01
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-04-08 17:21:46 +0300

    Added PasswordTextField component

---
+++ src/components/PasswordTextField/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { TextInput, Text, View } from 'react-native';
+
+import styles from './styles';
+
+const PasswordTextField = ({ term, placeHolder, onTermChange, onValidatePasswordField, error }) => {
+  return (
+    <View>
+      <Text style={styles.ErrorText}>{error}</Text>
+      <View style={styles.TextFieldView}>
+        <TextInput
+          autoCorrect={false}
+          secureTextEntry
+          style={styles.TextField}
+          placeholder={placeHolder}
+          value={term}
+          onChangeText={onTermChange}
+          onEndEditing={onValidatePasswordField}
+        />
+      </View>
+    </View>
+  );
+};
+
+export default PasswordTextField;
