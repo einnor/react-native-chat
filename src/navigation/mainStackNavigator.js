5eae7d14220e1a1437d361f58d36170296d6f7ac
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-04-08 16:31:38 +0300

    Created main stack navigator

---
+++ src/navigation/mainStackNavigator.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import SignInScreen from '../screens/SignIn';
+import GroupScreen from '../screens/Groups';
+import AddGroupScreen from '../screens/AddGroup';
+import ChatScreen from '../screens/Chat';
+
+const Stack = createStackNavigator();
+
+const ChatFlow = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator name="chat">
+        <Stack.Screen
+          name="SignInScreen"
+          component={SignInScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Groups Screen"
+          component={GroupScreen}
+          options={{ title: "Groups" }}
+        />
+        <Stack.Screen
+          name="Add Group Screen"
+          component={AddGroupScreen}
+          options={{ title: "Add Group" }}
+        />
+        <Stack.Screen
+          name="Chat Screen"
+          component={ChatScreen}
+          options={{ title: "Chats" }}
+        />
+
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+const mainStackNavigator = () => {
+  return (
+    ChatFlow()
+  );
+};
+
+export default mainStackNavigator;
