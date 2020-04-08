a6e04d2a361d7e9c9b487dba30523dc07215d453
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-04-08 17:41:12 +0300

    Added method to validate email address

--- src/screens/SignIn/index.js
+++ src/screens/SignIn/index.js
@@ -1,13 +1,30 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, Alert, SafeAreaView, Image, KeyboardAvoidingView } from 'react-native';
 
 import String from '../../consts/String';
 import Button from '../../components/Button';
 import TextField from '../../components/TextField';
 import PasswordTextField from '../../components/PasswordTextField';
+import Utility from '../../utils/Utility';
 import styles from './styles';
 
 const SignIn = () => {
+  const [state, setState] = useState({
+    email: '',
+    password: '',
+    emailError: '',
+    passwordError: '',
+    isLoading: false,
+  });
+
+  validateEmailAddress = () => {
+    const isValidEmail = Utility.isEmailValid(email);
+    isValidEmail ?
+      setState((prevState) => ({ ...prevState, emailError: '' }))
+      : setState((prevState) => ({ ...prevState, emailError: Strings.InvalidEmailAddress }));
+    return isValidEmail;
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.text} > SignIn Screen</Text>
--- src/screens/SignIn/styles.js
+++ src/screens/SignIn/styles.js
@@ -1,11 +1,17 @@
 import { StyleSheet } from 'react-native';
 
+import Constants from '../../consts/Constants';
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#ebebeb'
+  },
+  logo: {
+    alignSelf: 'center',
+    margin: 0.04 * Constants.screenHeight
   },
   text: {
     color: '#101010',
