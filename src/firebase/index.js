144a286d9f8b878c6209d6de3581c53380b5bea5
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-04-08 18:40:05 +0300

    Configure analytics

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -4,7 +4,7 @@ require('firebase/auth');
 
 import { FIREBASE_API_KEY, FIREBASE_AUTH_DOMAIN, FIREBASE_DATABASE_URL, FIREBASE_PROJECT_ID, FIREBASE_STORAGE_BUCKET, FIREBASE_MESSAGING_SENDER_ID, FIREBASE_APP_ID } from 'react-native-dotenv';
 
-var firebaseConfig = {
+const firebaseConfig = {
     apiKey: FIREBASE_API_KEY,
     authDomain: FIREBASE_AUTH_DOMAIN,
     databaseURL: FIREBASE_DATABASE_URL,
@@ -15,6 +15,7 @@ var firebaseConfig = {
 };
 
 firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
 export const firestore = firebase.firestore();
 export default firebase;
