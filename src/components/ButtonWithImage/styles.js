91d07edb168b98675f2f54665b6e9ccd49aa4e6d
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-04-09 11:26:10 +0300

    Added styles for button with image component

---
+++ src/components/ButtonWithImage/styles.js
@@ -0,0 +1,21 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  button: {
+    display: 'flex',
+    height: 30,
+    width: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 10
+  },
+  ImageIconStyle: {
+    padding: 10,
+    margin: 5,
+    height: 25,
+    width: 25,
+    resizeMode: 'stretch'
+  },
+});
+
+export default styles;
