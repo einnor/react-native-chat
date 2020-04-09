bc5178aff9fd70322effe53e8ed28a424406a69f
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-04-09 12:06:26 +0300

    Validate group name

--- src/screens/AddGroup/index.js
+++ src/screens/AddGroup/index.js
@@ -1,9 +1,23 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Alert } from 'react-native';
 
 import styles from './styles';
 
 const AddGroup = () => {
+  const [state, setState] = useState({
+    name: '',
+    error: '',
+    isLoading: false,
+  });
+
+  validateField = () => {
+    const isValidField = Utility.isValidField(state.name)
+    isValidField ?
+      setState((prevState) = ({ ...prevState, error: '' }))
+      : setState((prevState) => ({ ...prevState, error: Strings.GroupNameEmpty }));
+    return isValidField;
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.text} > Add Group</Text>
