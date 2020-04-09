818641f8a9fa460b7b0455a02d1a97126a0bf336
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-04-09 11:40:27 +0300

    Navigate to chat screen on press of group

--- src/screens/Groups/index.js
+++ src/screens/Groups/index.js
@@ -1,11 +1,26 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useLayoutEffect, useState, useEffect } from 'react';
+import { View, Text, FlatList, TouchableOpacity, Alert } from 'react-native';
 
+import GroupItem from '../../components/GroupItem';
 import styles from './styles';
 
-const Groups = () => {
+const Groups = ({ navigation }) => {
+  const [groups, setGroups] = useState([]);
+
+  navigateToChatScreen = (item) => {
+    navigation.navigate('Chat Screen', { item })
+  }
   return (
     <View style={styles.container}>
+      <FlatList
+        data={groups}
+        keyExtractor={(item, index) => `key-${index}`}
+        renderItem={({ item }) => (
+          <TouchableOpacity onPress={() => navigateToChatScreen(item)}>
+            <GroupItem item={item}></GroupItem>
+          </TouchableOpacity>
+        )}
+      />
       <Text style={styles.text} > Groups Screen</Text>
     </View>
   );
