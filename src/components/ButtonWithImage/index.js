ee8b33e7813d7a17bdabe9a45858c20ee0ee5cbd
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-04-09 11:26:51 +0300

    Added button with image component

---
+++ src/components/ButtonWithImage/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { TouchableOpacity, Image, View } from 'react-native';
+
+import styles from './styles';
+
+const ButtonWithImage = (props) => {
+  const { style = {}, onPress, image } = props;
+
+  return (
+    <View style={[styles.button, style]}>
+      <TouchableOpacity onPress={onPress}>
+        <Image
+          source={image}
+          style={styles.ImageIconStyle}
+        />
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default ButtonWithImage;
