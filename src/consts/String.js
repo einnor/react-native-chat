e27dc3bf501d6f5008bbd9db9352b2b5cc84ef17
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-04-09 12:09:48 +0300

    Updated string contants

--- src/consts/String.js
+++ src/consts/String.js
@@ -3,7 +3,10 @@ const Strings = {
   InvalidEmailAddress: 'Invalid Email Address !',
   PasswordNotStrong: 'Password must at least 8 character long, and must contain at least - 1 lowercase alphabetical character, 1 uppercase alphabetical character, 1 numeric character and 1 special character',
   EmailPlaceHolder: 'Email Address',
-  PasswordPlaceHolder: 'Password'
+  PasswordPlaceHolder: 'Password',
+  EnterYourGroupName: 'Enter your group name',
+  GroupNameEmpty: 'Group Name cannot be empty!',
+  CreateGroup: 'Create Group'
 }
 
 export default Strings;
