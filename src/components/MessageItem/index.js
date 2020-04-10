d0075481ce7742343aadb1ab4ce686f44114c4d7
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-04-10 12:07:40 +0300

    Added MessageItem component

---
+++ src/components/MessageItem/index.js
@@ -0,0 +1,35 @@
+/* eslint-disable react-native/no-inline-styles */
+import React from 'react';
+import { View, Text } from 'react-native';
+
+import firebase from '../firebase/Firebase';
+import styles from './styles';
+
+const MessageItem = ({ item }) => {
+  const userID = firebase.auth().currentUser.uid;
+
+  const messageView = () => {
+    if (userID === item.senderId) {
+      return (
+        <View style={styles.othersMessageContainerView}>
+          <Text style={[styles.senderName, { textAlign: 'right' }]}>
+            {item.senderEmail}
+          </Text>
+          <Text style={[styles.message, { textAlign: 'right' }]}>
+            {item.message}
+          </Text>
+        </View>
+      );
+    }
+    return (
+      <View style={styles.myMessageContainerView}>
+        <Text style={styles.senderName}> {item.senderEmail}</Text>
+        <Text style={styles.message}>{item.message}</Text>
+      </View>
+    );
+  };
+
+  return messageView();
+};
+
+export default MessageItem;
