2d21d53277cfd01414ea658ef56c287a30666ffb
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-04-10 12:25:45 +0300

    Determine if the user has already joined the group or not

--- src/screens/Chat/index.js
+++ src/screens/Chat/index.js
@@ -1,9 +1,50 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text } from 'react-native';
 
+import firebase, { firestore } from '../../firebase';
 import styles from './styles';
 
-const Chat = () => {
+const Chat = ({ navigation, route }) => {
+  const [state, setState] = useState({
+    messages: [],
+    message: '',
+    isJoined: false,
+  });
+
+  const { item } = route.params;
+  const userID = firebase.auth().currentUser.uid;
+
+  const getUserJoinedAlreadyOrNot = () => {
+    firestore
+      .collection('members')
+      .doc(item.groupID)
+      .collection('member')
+      .where('userID', '==', userID)
+      .get()
+      .then((querySnapshot) => {
+        if (querySnapshot.size > 0) {
+          querySnapshot.forEach((doc) => {
+            if (doc.data() != null) {
+              setState((prevState) => ({ ...prevState, isJoined: true }));
+            } else {
+              setState((prevState) => ({ ...prevState, isJoined: true }));
+              showAlertToJoinGroup();
+            }
+          });
+        } else {
+          showAlertToJoinGroup();
+        }
+      })
+      .catch((error) => {
+        console.log('Error getting documents: ', error);
+      });
+  };
+
+  useEffect(() => {
+    getUserJoinedAlreadyOrNot();
+    getMessages();
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text style={styles.text}> Chat Screen</Text>
